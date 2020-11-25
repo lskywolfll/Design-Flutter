7886e11270f14438d68ae788beadc80dfd7632d9
Rene <renealejandrosanchezmorales@gmail.com>
2020-11-24 22:56:48 -0300

    se termina la pagina 2 con optimizaciones mejores

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -58,22 +58,41 @@ class ScrollPage extends StatelessWidget {
 
   Widget _pagina2() {
     return Container(
-      padding: EdgeInsets.all(10.0),
-      color: Colors.cyan[300],
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
       child: Center(
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(30.0),
-          child: FlatButton(
-            padding: EdgeInsets.symmetric(horizontal: 60.0, vertical: 12.0),
-            textColor: Colors.white,
-            onPressed: () {},
-            color: Colors.blue,
-            child: Text(
-              "Bienvenido",
-            ),
+        child: RaisedButton(
+          onPressed: () {},
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+            child: Text("Bienvenidos", style: TextStyle(fontSize: 20.0)),
           ),
+          shape: StadiumBorder(),
+          textColor: Colors.white,
+          color: Colors.blue,
         ),
       ),
     );
+
+    //   return Container(
+    //     padding: EdgeInsets.all(10.0),
+    //     color: Colors.cyan[300],
+    //     child: Center(
+    //       child: ClipRRect(
+    //         borderRadius: BorderRadius.circular(30.0),
+    //         child: FlatButton(
+    //           padding: EdgeInsets.symmetric(horizontal: 60.0, vertical: 12.0),
+    //           textColor: Colors.white,
+    //           onPressed: () {},
+    //           color: Colors.blue,
+    //           child: Text(
+    //             "Bienvenido",
+    //           ),
+    //         ),
+    //       ),
+    //     ),
+    //   );
+    // }
   }
 }
